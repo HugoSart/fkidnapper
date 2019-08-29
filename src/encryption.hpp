a83224ec92c20ab24458796bc7ebf4be197d4e45
Hugo Sartori <hugo_sart@hotmail.com>
2019-08-28 22:38:11 -0300

    Update encryption.hpp

--- src/encryption.hpp
+++ src/encryption.hpp
@@ -3,21 +3,19 @@
 
 #include <iostream>
 
-static const long HASH_CODE = 159785L;
-
 /**
  * Encrypt a 4 byte number
  **/
 uint32_t encrypt(uint32_t i)
 {
-    return i += HASH_CODE;
+    return ~i;
 }
 
 /**
  * Descrypt a 4 byte number
  **/
 uint32_t decrypt(uint32_t i) {
-    return i -= HASH_CODE;
+    return ~i;
 }
 
 #endif // __ENCRIPTION_HPP
